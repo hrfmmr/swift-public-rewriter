e77693cd904e2b385d188f5d85c4b30adb4475ee
hrfmmr <i@hrfmmr.com>
2023-12-24 11:07:39 +0900

    wip

--- Sources/main/PublicModifierRewriter.swift
+++ Sources/main/PublicModifierRewriter.swift
@@ -2,7 +2,7 @@ import SwiftSyntax
 
 private extension VariableDeclSyntax {
     /// Whether `private(set) var ...`
-    var hasPrivateSet: Bool {
+    var hasPrivateSetVar: Bool {
         let modifiers = Array(modifiers)
         guard modifiers.count == 1 else { return false }
         let modifier = modifiers[0]
@@ -10,47 +10,93 @@ private extension VariableDeclSyntax {
         else { return false }
         return true
     }
+    
+    /// Whether `lazy var ...`
+    var hasLazyVar: Bool {
+        let modifiers = Array(modifiers)
+        guard modifiers.count == 1 else { return false }
+        let modifier = modifiers[0]
+        guard modifier.name.text == "lazy" else { return false }
+        return true
+    }
+    
+    static func makeNewPublicModifiers(from node: VariableDeclSyntax) -> DeclModifierListSyntax? {
+        guard let pattern = MakePublicDeclPattern.from(node) else { return nil }
+        let publicModifier = DeclModifierSyntax(
+            leadingTrivia: .newlines(1),
+            name: .keyword(.public),
+            trailingTrivia: .spaces(1)
+        )
+        var modifiers = DeclModifierListSyntax([
+            publicModifier,
+        ])
+        
+        switch pattern {
+        case .defaultInternal:
+            break
+        case .internalPrivateSetVar:
+            let existingModifier = DeclModifierSyntax(
+                name: .keyword(.private),
+                trailingTrivia: .spaces(1)
+            )
+            modifiers.append(existingModifier.with(\.detail, Array(node.modifiers)[0].detail))
+        case .internalLazyVar:
+            let existingModifier = DeclModifierSyntax(
+                name: .keyword(.lazy),
+                trailingTrivia: .spaces(1)
+            )
+            modifiers.append(existingModifier)
+        }
+        return modifiers
+    }
+    
+    
+    enum MakePublicDeclPattern {
+        /// No access scope(internal)
+        /// `let ...`
+        /// `var ...`
+        case defaultInternal
+        
+        /// internal scope of `private(set) var ...`
+        case internalPrivateSetVar
+        
+        /// internal scope of `lazy var ...`
+        case internalLazyVar
+        
+        static func from(_ node: VariableDeclSyntax) -> Self? {
+            if node.hasPrivateSetVar {
+                .internalPrivateSetVar
+            } else if node.hasLazyVar {
+                .internalLazyVar
+            } else if node.modifiers.isEmpty {
+                .defaultInternal
+            } else {
+                nil
+            }
+        }
+    }
 }
 
 class PublicModifierRewriter: SyntaxRewriter {
     override func visit(_ node: VariableDeclSyntax) -> DeclSyntax {
-        guard let parent = node.parent, parent.is(MemberBlockItemSyntax.self)
+        guard 
+            let parent = node.parent,
+            parent.is(MemberBlockItemSyntax.self)
         else { return super.visit(node) }
         
-        if node.hasPrivateSet == false || node.modifiers.isEmpty {
-            return super.visit(node)
-        }
+        guard
+            let newModifiers = VariableDeclSyntax.makeNewPublicModifiers(from: node),
+            case let .keyword(keyword) = node.bindingSpecifier.tokenKind
+        else { return super.visit(node) }
+
         print("🟠node(VariableDeclSyntax):\(node.description)")
         
-        guard case let .keyword(keyword) = node.bindingSpecifier.tokenKind else { return super.visit(node) }
-
-        let newModifiers: DeclModifierListSyntax = {
-            let publicModifier = DeclModifierSyntax(
-                leadingTrivia: .newlines(1),
-                name: .keyword(.public),
-                trailingTrivia: .spaces(1)
-            )
-            var modifiers = DeclModifierListSyntax([
-                publicModifier
-            ])
-            if node.hasPrivateSet {
-                let existingModifier = DeclModifierSyntax(
-                    name: .keyword(.private),
-                    trailingTrivia: .spaces(1)
-                )
-                modifiers.append(existingModifier.with(\.detail, Array(node.modifiers)[0].detail))
-            }
-            return modifiers
-        }()
-        
-        let newNode = node.with(
-            \.bindingSpecifier,
-             .keyword(keyword, trailingTrivia: .spaces(1))
-        )
+        let newNode = node
+            .with(\.bindingSpecifier, .keyword(keyword, trailingTrivia: .spaces(1)))
             .with(\.modifiers, newModifiers)
         return super.visit(newNode)
     }
-
+    
     override func visit(_ node: ClassDeclSyntax) -> DeclSyntax {
         if node.modifiers.contains(where: { $0.name.text == "public" }) {
             return super.visit(node)
