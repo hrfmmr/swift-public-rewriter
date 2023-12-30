0af6bb13c3272358c3fc7e1b6d2c37ea4b4a9e48
hrfmmr <i@hrfmmr.com>
2023-12-30 22:49:23 +0900

    Add tests

---
+++ Tests/PublicRewriterTests/EnumDeclSyntaxTests.swift
@@ -0,0 +1,47 @@
+import XCTest
+
+import SwiftSyntax
+import SwiftParser
+
+@testable import PublicRewriter
+
+class EnumDeclSyntaxTests: XCTestCase {
+    func testRewriter() throws {
+        let source = """
+enum Test {
+    case a, b, c
+    var string: String {
+        switch self {
+        case .a: return "a"
+        case .b: return "b"
+        case .c: return "c"
+        }
+    }
+    
+    func makeString() -> String {
+        string
+    }
+}
+"""
+        let expected = """
+public enum Test {
+    case a, b, c
+    public var string: String {
+        switch self {
+        case .a: return "a"
+        case .b: return "b"
+        case .c: return "c"
+        }
+    }
+    
+    public func makeString() -> String {
+        string
+    }
+}
+"""
+        let sourceFile = Parser.parse(source: source)
+        let rewriter = PublicModifierRewriter()
+        let modifiedSource = rewriter.visit(sourceFile)
+        XCTAssertEqual(modifiedSource.description, expected)
+    }
+}
