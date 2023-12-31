542e98c05d88e381a6c4a5995ace4f275dd20580
hrfmmr <i@hrfmmr.com>
2023-12-31 22:15:32 +0900

    Refactor Package.swift

--- Package.swift
+++ Package.swift
@@ -9,24 +9,58 @@ let package = Package(
         .macOS(.v10_15),
     ],
     products: [
-        .library(name: "PublicRewriter", targets: ["PublicRewriter"]),
+        .publicRewriter,
     ],
     dependencies: [
-      .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.2"),
+        .swiftSyntax
     ],
     targets: [
-        .target(
-            name: "PublicRewriter",
-            dependencies: [
-                .product(name: "SwiftSyntax", package: "swift-syntax"),
-                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
-            ]
-        ),
-        .testTarget(
-            name: "PublicRewriterTests",
-            dependencies: [
-                "PublicRewriter",
-            ]
-        ),
+        .publicRewriter,
+        .publicRewriterTests,
     ]
 )
+
+private extension PackageDescription.Package.Dependency {
+    typealias _Self = PackageDescription.Package.Dependency
+    static let swiftSyntax: _Self = .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.2")
+}
+
+private extension PackageDescription.Product {
+    typealias _Self = PackageDescription.Product
+    static let publicRewriter: _Self = .library(
+        name: .LocalTarget.publicRewriter,
+        targets: [.LocalTarget.publicRewriter]
+    )
+}
+
+private extension PackageDescription.Target {
+    typealias _Self = PackageDescription.Target
+    static let publicRewriter: _Self = .target(
+        name: .LocalTarget.publicRewriter,
+        dependencies: [
+            .swiftSyntax,
+            .swiftSyntaxBuilder,
+        ]
+    )
+    static let publicRewriterTests: _Self = .testTarget(
+        name: .LocalTarget.publicRewriterTests,
+        dependencies: [.publicRewriter]
+    )
+
+}
+
+private extension PackageDescription.Target.Dependency {
+    // Third party libs
+    static let swiftSyntax: Self = .product(name: "SwiftSyntax", package: "swift-syntax")
+    static let swiftSyntaxBuilder: Self = .product(name: "SwiftSyntaxBuilder", package: "swift-syntax")
+    
+    // Local targets
+    static let publicRewriter: Self = .target(name: .LocalTarget.publicRewriter)
+}
+
+private extension String {
+    enum LocalTarget {
+        static let publicRewriter = "PublicRewriter"
+        static let publicRewriterTests = "PublicRewriterTests"
+    }
+}
